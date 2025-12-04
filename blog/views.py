304ddb57df3bcfbf5ed6ae0e681942a2abee62e8
Felipe Fratti <felipe.fratti@hotmail.com>
2025-12-04 00:37:24 -0300

    Versão 2: Views funcionais com Django Forms

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from .models import Post
+from .forms import PostForm  # <--- NOVA IMPORTAÇÃO AQUI
 
-# 1. Listar (Read)
+# 1. Listar (Não muda)
 def post_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-# 2. Detalhes (Read) com erro 404 manual
+# 2. Detalhes (Não muda)
 def post_detail(request, pk):
     try:
         post = Post.objects.get(pk=pk)
@@ -15,27 +16,32 @@ def post_detail(request, pk):
         raise Http404("Post não encontrado")
     return render(request, 'blog/post_detail.html', {'post': post})
 
-# 3. Criar (Create) - Manualmente pegando dados do HTML
+# 3. Criar (ALTERADO PARA USAR FORM)
 def post_create(request):
     if request.method == "POST":
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        # Cria e salva no banco
-        Post.objects.create(title=title, content=content)
-        return redirect('post_list')
-    return render(request, 'blog/post_form.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
 
-# 4. Atualizar (Update) - Manualmente
+# 4. Atualizar (ALTERADO PARA USAR FORM)
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.save()
-        return redirect('post_detail', pk=post.pk)
-    return render(request, 'blog/post_form.html', {'post': post})
+        # Preenchemos o form com os dados novos (POST) e dizemos quem estamos editando (instance)
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        # Preenchemos o form com os dados atuais do post
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
 
-# 5. Deletar (Delete) - Com confirmação
+# 5. Deletar (Não muda na Versão 2)
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
